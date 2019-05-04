368dd52eeede9955f814d8134f34d85f0aa3f2ed
Abid Shamim P.M <abid.shamim.p.m@ing.com>
2019-05-04 16:04:02 +0200

    Updates

---
+++ precache-manifest.4be251bc5c87c49666d1bedab30d58ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6238423af0af254cd193688d28ae9d82",
+    "url": "/static/media/chewing.6238423a.gif"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f5e816bd44504a89e03a",
+    "url": "/static/js/main.76819549.chunk.js"
+  },
+  {
+    "revision": "07387066bf24ff47e088",
+    "url": "/static/js/2.d7ac017a.chunk.js"
+  },
+  {
+    "revision": "f5e816bd44504a89e03a",
+    "url": "/static/css/main.5279f9ba.chunk.css"
+  },
+  {
+    "revision": "8d835a490f65736cc4c8700b1a69b311",
+    "url": "/index.html"
+  }
+];
